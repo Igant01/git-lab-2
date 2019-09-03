7c53a368313eeedcb99fe235504f7310434023cd
Ian Gant <ig622218@ohio.edu>
2019-09-03 17:22:46 -0400

    C++ Program

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Ian Gant
+ *        Date: 9/3/19
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    
+    /*add code*/
+    return 0;
+}// main
